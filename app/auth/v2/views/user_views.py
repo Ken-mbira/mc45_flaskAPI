37ce82635953fdd36929ad413533c0520588aeda
ken <ken.mbira@student.moringaschool.com>
2021-09-23 09:19:53 +0300

    add a request to get the data from the inputs

--- app/auth/v2/views/user_views.py
+++ app/auth/v2/views/user_views.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restful import Resource, reqparse
 from app.auth.v2.models.user_models import UserModel
 
@@ -18,22 +19,20 @@ class UserRegister(Resource):
         """
         try:
             args = UserRegister.parser.parse_args()
-            username = args.get('username')
-            email = args.get('email')
-            password = args.get('password')
-            confirm_password = args.get('confirm_password')
+            args = request.get_json()
+            new_user = user_model_view.create_user(
+                username=args['username'],
+                email=args['email'],
+                password=args['password'],
+                confirm_password=args['confirm_password']
+                )
+
+
         except Exception as e:
             return {
                 "status": 400,
                 "error": "Invalid Key error {}".format(e)
             }, 400
-
-        new_user = user_model_view.create_user(
-            username=args.get('username'),
-            email=args.get('email'),
-            password=args.get('password'),
-            confirm_password=args.get('confirm_password')
-        )
 
         return {
             "status": 201,
